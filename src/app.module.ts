50daa953b752d60382c97d4d936b7aef2530c034
long <nvl1682@example.com>
2026-07-23 21:09:57 +0700

    feat: add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { DatabaseModule } from './database/database.module';
 import { WorkoutModule } from './features/workout/workout.module';
+import { UserModule } from './features/user/user.module';
 
 @Module({
-  imports: [DatabaseModule, CqrsModule.forRoot(), WorkoutModule],
+  imports: [DatabaseModule, CqrsModule.forRoot(), WorkoutModule, UserModule],
   controllers: [],
   providers: [],
 })
